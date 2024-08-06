529c50a57761c07c38ec7406a3f259fb910e9067
attaphol1 <attaphol.school@gmail.com>
2024-08-07 01:05:31 +0700

    test

--- BalanceTracker.py
+++ BalanceTracker.py
@@ -167,7 +167,6 @@ def plot_and_save_data():
 
     # Plot in main thread using after
     root.after(0, lambda: plot_3d_data(x_data, y_data, z_data))
-
 def plot_3d_data(x_data, y_data, z_data):
     fig = plt.figure(figsize=(5, 5))
     ax = fig.add_subplot(111, projection='3d')
