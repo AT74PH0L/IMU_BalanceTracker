345d7c047df473d9d196467ccf16aa09ee43c463
attaphol1 <attaphol.school@gmail.com>
2025-07-04 16:53:31 +0700

    -

--- BalanceTracker.py
+++ BalanceTracker.py
@@ -21,7 +21,8 @@ signal.signal(signal.SIGINT, signal_handler)
 folder_selected = False
 bluetooth_connected = False
 
-data_queue = queue.Queue()
+data_gyro = queue.Queue()
+data_acce = queue.Queue()
 collecting = False
 start_time = 0
 duration = 0
@@ -32,22 +33,75 @@ address = "C0:8A:EF:C8:ED:07"
 
 client = None
 
-async def read_sensor_data():
-    global collecting
+# async def read_sensor_data():
+#     global collecting
+#     while collecting:
+#         if bluetooth_connected and client and client.is_connected:
+#             try:
+#                 raw_Acce_x = await client.read_gatt_char("1e182bb7-f3fd-4240-bc91-aabb9436c0b7")
+#                 raw_Acce_y = await client.read_gatt_char("3198a4f7-e0da-4ec0-aafe-d978201bdcaa")
+#                 raw_Acce_z = await client.read_gatt_char("4a3c7e31-deae-43ed-90a3-01a71c7ad28b")
+
+#                 raw_gyro_x = await client.read_gatt_char("5c697a4f-7590-47cb-9b90-90d7732a7529")
+#                 raw_gyro_y = await client.read_gatt_char("21680407-7051-434a-8fb5-362ea1c01916")
+#                 raw_gyro_z = await client.read_gatt_char("53acd7ae-09f2-4015-9bfc-092342c68b1d")
+                
+#                 gyro_x_str = raw_gyro_x.decode('utf-8').strip().replace(',', '')
+#                 gyro_y_str = raw_gyro_y.decode('utf-8').strip().replace(',', '')
+#                 gyro_z_str = raw_gyro_z.decode('utf-8').strip().replace(',', '')
+
+#                 Acce_x_str = raw_Acce_x.decode('utf-8').strip().replace(',', '')
+#                 Acce_y_str = raw_Acce_y.decode('utf-8').strip().replace(',', '')
+#                 Acce_z_str = raw_Acce_z.decode('utf-8').strip().replace(',', '')
+
+#                 update_timer()
+#                 gyro_x = float(gyro_x_str)
+#                 gyro_y = float(gyro_y_str)
+#                 gyro_z = float(gyro_z_str)
+
+#                 acce_x = float(Acce_x_str)
+#                 acce_y = float(Acce_y_str)
+#                 acce_z = float(Acce_z_str)
+#                 data_gyro.put((gyro_x, gyro_y, gyro_z))
+#                 data_acce.put((acce_x, acce_y, acce_z))
+#             except Exception as e:
+#                 print(f"Error reading sensor data: {e}")
+#         await asyncio.sleep(0.1)
+
+# ปรับปรุงฟังก์ชัน read_sensor_data() เพื่อให้เลือกเก็บข้อมูลตามที่เลือกใน Dropdown
+async def read_sensor_data(sensor_type):
     while collecting:
         if bluetooth_connected and client and client.is_connected:
             try:
-                raw_gyro_x = await client.read_gatt_char("1e182bb7-f3fd-4240-bc91-aabb9436c0b7")
-                raw_gyro_y = await client.read_gatt_char("3198a4f7-e0da-4ec0-aafe-d978201bdcaa")
-                raw_gyro_z = await client.read_gatt_char("4a3c7e31-deae-43ed-90a3-01a71c7ad28b")
-                gyro_x_str = raw_gyro_x.decode('utf-8').strip().replace(',', '')
-                gyro_y_str = raw_gyro_y.decode('utf-8').strip().replace(',', '')
-                gyro_z_str = raw_gyro_z.decode('utf-8').strip().replace(',', '')
-                update_timer()
-                gyro_x = float(gyro_x_str)
-                gyro_y = float(gyro_y_str)
-                gyro_z = float(gyro_z_str)
-                data_queue.put((gyro_x, gyro_y, gyro_z))
+                if sensor_type == "gyro":
+
+                    raw_gyro_x = await client.read_gatt_char("5c697a4f-7590-47cb-9b90-90d7732a7529")
+                    raw_gyro_y = await client.read_gatt_char("21680407-7051-434a-8fb5-362ea1c01916")
+                    raw_gyro_z = await client.read_gatt_char("53acd7ae-09f2-4015-9bfc-092342c68b1d")
+
+                    gyro_x_str = raw_gyro_x.decode('utf-8').strip().replace(',', '')
+                    gyro_y_str = raw_gyro_y.decode('utf-8').strip().replace(',', '')
+                    gyro_z_str = raw_gyro_z.decode('utf-8').strip().replace(',', '')
+                    update_timer()
+                    gyro_x = float(gyro_x_str)
+                    gyro_y = float(gyro_y_str)
+                    gyro_z = float(gyro_z_str)
+                    data_gyro.put((gyro_x, gyro_y, gyro_z))
+
+                else:  # sensor_type == "acce"
+                    raw_Acce_x = await client.read_gatt_char("1e182bb7-f3fd-4240-bc91-aabb9436c0b7")
+                    raw_Acce_y = await client.read_gatt_char("3198a4f7-e0da-4ec0-aafe-d978201bdcaa")
+                    raw_Acce_z = await client.read_gatt_char("4a3c7e31-deae-43ed-90a3-01a71c7ad28b")
+
+                    Acce_x_str = raw_Acce_x.decode('utf-8').strip().replace(',', '')
+                    Acce_y_str = raw_Acce_y.decode('utf-8').strip().replace(',', '')
+                    Acce_z_str = raw_Acce_z.decode('utf-8').strip().replace(',', '')
+                    update_timer()
+                    acce_x = float(Acce_x_str)
+                    acce_y = float(Acce_y_str)
+                    acce_z = float(Acce_z_str)
+                    data_acce.put((acce_x, acce_y, acce_z))
+
             except Exception as e:
                 print(f"Error reading sensor data: {e}")
         await asyncio.sleep(0.1)
@@ -68,6 +122,7 @@ async def init_bluetooth_connection():
     except Exception as e:
         connection_status.set(f"Connection error: {e}")
 
+# ปรับปรุงฟังก์ชัน start_collection() เพื่อใช้ค่าจาก Dropdown
 def start_collection():
     global collecting, start_time, duration, canvas, folder_path
     if canvas:
@@ -78,6 +133,8 @@ def start_collection():
     duration = int(duration_entry.get())  # Get duration from entry field
     start_button.config(state=tk.DISABLED)
     
+    # Use asyncio.run() to run the async function
+
     # Ensure folder path is selected
     if not folder_selected:
         messagebox.showerror("Error", "No folder selected. Please select a folder to save the data.")
@@ -86,11 +143,16 @@ def start_collection():
 
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     global folder_path_data
-    folder_path_data = folder_path + "/" + timestamp
+    folder_path_data = folder_path+"/"+timestamp
+    print('in start ',folder_path_data)
     os.makedirs(folder_path_data, exist_ok=True)
 
-    # Start the data collection thread
-    threading.Thread(target=lambda: asyncio.run(read_sensor_data())).start()
+    if data_type.get() == "Gyro":
+        # asyncio.run(read_sensor_data("gyro"))
+        threading.Thread(target=lambda: asyncio.run(read_sensor_data("gyro"))).start()
+    else:
+        threading.Thread(target=lambda: asyncio.run(read_sensor_data("acce"))).start()
+    #-----------------------------------------------------------------------------
 
 def stop_collection():
     global collecting
@@ -148,25 +210,31 @@ def save_axis_plots(x_data, y_data, z_data):
 
 def plot_and_save_data():
     gyro_data = []
-    while not data_queue.empty():
-        gyro_data.append(data_queue.get())
+    acce_data = []
+    while not data_gyro.empty() and not data_acce.empty():
+        gyro_data.append(data_gyro.get())
+        acce_data.append(data_acce.get())
 
     if not gyro_data:
         messagebox.showinfo("No Data", "No data to plot.")
         return
 
-    x_data = [data[0] for data in gyro_data]
-    y_data = [data[1] for data in gyro_data]
-    z_data = [data[2] for data in gyro_data]
+    x_gyro = [data[0] for data in gyro_data]
+    y_gyro = [data[1] for data in gyro_data]
+    z_gyro = [data[2] for data in gyro_data]
+
+    x_acco = [data[0] for data in gyro_data]
+    y_acco = [data[1] for data in gyro_data]
+    z_acco = [data[2] for data in gyro_data]
 
     # Save raw data
     save_raw_data(gyro_data)
 
     # Save axis plots
-    save_axis_plots(x_data, y_data, z_data)
+    save_axis_plots(x_gyro, y_gyro, z_gyro)
 
     # Plot in main thread using after
-    root.after(0, lambda: plot_3d_data(x_data, y_data, z_data))\
+    root.after(0, lambda: plot_3d_data(x_gyro, y_gyro, z_gyro))\
     
 def plot_3d_data(x_data, y_data, z_data):
     fig = plt.figure(figsize=(5, 5))
@@ -237,26 +305,39 @@ duration_entry.grid(row=0, column=1, padx=5, pady=5)
 start_button = ttk.Button(main_frame, text="Start", command=lambda: countdown(3), state=tk.DISABLED)
 start_button.grid(row=2, column=0, padx=5, pady=5)
 
+stop_button = ttk.Button(main_frame, text="Stop", command=stop_collection, state=tk.DISABLED)
+stop_button.grid(row=2, column=1, padx=5, pady=5)
+
+data_type_label = ttk.Label(main_frame, text="Select Data Type:")
+data_type_label.grid(row=1, column=0, padx=5, pady=5)
+
+data_type = tk.StringVar(value="Gyro")  # ค่าปริยายเป็น Gyro
+data_type_combobox = ttk.Combobox(main_frame, textvariable=data_type, values=["Gyro", "Acce"])
+data_type_combobox.grid(row=1, column=1, padx=5, pady=5)
+data_type_combobox.current(0)  # ตั้งค่าเป็น Gyro โดยปริยาย
+
 timer_label = ttk.Label(main_frame, text="Time elapsed: 0.0 seconds")
-timer_label.grid(row=2, column=1, columnspan=2, padx=5, pady=5)
+timer_label.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
 
 folder_button = ttk.Button(main_frame, text="Select Folder", command=select_folder)
-folder_button.grid(row=3, column=0,padx=5, pady=5)
+folder_button.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
 
 folder_path_name_label = ttk.Label(main_frame, text="Path: Not selected")
-folder_path_name_label.grid(row=3, column=1)
+folder_path_name_label.grid(row=5, column=0, columnspan=2, padx=5, pady=5)
 
+# Connection Status Label
 connection_status = tk.StringVar()
-connection_status.set("Bluetooth Connecting...")
+connection_status.set("Bluetooth not connected.")
 connection_status_label = ttk.Label(main_frame, textvariable=connection_status)
 connection_status_label.grid(row=6, column=0, columnspan=2, padx=5, pady=5)
 
-version_label = tk.Label(root, text="Version 1.1.0", font=("Arial", 8))
+# สร้างเลขเวอร์ชัน
+version_label = tk.Label(root, text="Version 1.0.3", font=("Arial", 8))
 version_label.place(relx=1.0, rely=0.0, anchor='ne', x=-10, y=10)
 
+# การตั้งค่ากราฟ
 graph_frame = ttk.Frame(root, padding="10")
 graph_frame.grid(row=1, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
-
 def init_bluetooth():
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
